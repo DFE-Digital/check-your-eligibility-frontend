31cd7cd757e429a1ebaf8b182a0647c83592a94c
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-05-09 13:02:05 +0100

    refactoured

---
+++ CheckYourEligibility-FrontEnd.Services/EcsServiceAdmin.cs
@@ -0,0 +1,28 @@
+﻿using CheckYourEligibility.Domain.Requests;
+using CheckYourEligibility.Domain.Responses;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace CheckYourEligibility_FrontEnd.Services
+{
+    public class EcsServiceAdmin : BaseService,  IEcsServiceAdmin
+    {
+        private readonly ILogger _logger;
+        private readonly HttpClient _httpClient;
+        private readonly string _FsmUrl;
+        private readonly string _schoolUrl;
+
+        public EcsServiceAdmin(ILoggerFactory logger, HttpClient httpClient,IConfiguration configuration): base("EcsService", logger, httpClient, configuration)
+        {
+            _logger = logger.CreateLogger("EcsService");
+            _httpClient = httpClient;
+            _FsmUrl = configuration["EcsFsmControllerUrl"];
+            _schoolUrl = configuration["EcsFsmSchoolUrl"];
+        }
+
+    }
+}
