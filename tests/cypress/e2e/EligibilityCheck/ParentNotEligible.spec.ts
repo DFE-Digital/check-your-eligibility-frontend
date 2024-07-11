9d2d93bc4e8e5b75c256d1b29bfbc873cc5c4de2
Sam Fallowfield <sam.fallowfield@education.gov.uk>
2024-07-11 11:52:52 +0100

    added enter child details test and updated not eligible tests

--- tests/cypress/e2e/EligibilityCheck/ParentNotEligible.spec.ts
+++ tests/cypress/e2e/EligibilityCheck/ParentNotEligible.spec.ts
@@ -2,7 +2,6 @@
 describe('Parents journey when not eligible', () => {
 
     it('Will not return the correct responses if the Parent is not eligible for free school meals', () => {
-
         cy.visit('/');
         cy.get('h1').should('include.text', 'Check if your children can get free school meals');
         cy.contains('Start Now').click();
@@ -26,7 +25,6 @@ describe('Parents journey when not eligible', () => {
     });
 
     it('Will return the correct response if we cannot find the user', () => {
-
         cy.visit('/');
         cy.get('h1').should('include.text', 'Check if your children can get free school meals');
         cy.contains('Start Now').click();
@@ -48,5 +46,79 @@ describe('Parents journey when not eligible', () => {
         cy.url().should('include', '/Check/Loader');
         cy.get('h1').should('include.text', "We could not check your children’s entitlement to free school meals");
     });
-    
+
+    it('Will return the correct error response if the user inputs a NI number in the incorrect format', () => {
+        cy.visit('/');
+        cy.get('h1').should('include.text', 'Check if your children can get free school meals');
+        cy.contains('Start Now').click();
+
+        cy.url().should('include', '/Check/Enter_Details')
+        cy.get('h1').should('include.text', 'Enter your details');
+        cy.get('#FirstName').type('Tim');
+        cy.get('#LastName').type('Smith');
+
+        cy.get('#Day').type('01');
+        cy.get('#Month').type('01');
+        cy.get('#Year').type('1990');
+
+        cy.get('#IsNassSelected').click();
+        cy.get('#NationalInsuranceNumber').type('ABCDE456C');
+
+        cy.contains('Save and continue').click();
+
+        cy.get('h2').should('include.text', 'There is a problem');
+        cy.get('a').should('include.text', 'Invalid National Insurance Number format');
+    });
+
+    it('Will return the correct error response if the user inputs a NI number is too long', () => {
+        cy.visit('/');
+        cy.get('h1').should('include.text', 'Check if your children can get free school meals');
+        cy.contains('Start Now').click();
+
+        cy.url().should('include', '/Check/Enter_Details');
+        cy.get('h1').should('include.text', 'Enter your details');
+        cy.get('#FirstName').type('Tim');
+        cy.get('#LastName').type('Smith');
+
+        cy.get('#Day').type('01');
+        cy.get('#Month').type('01');
+        cy.get('#Year').type('1990');
+
+        cy.get('#IsNassSelected').click();
+        cy.get('#NationalInsuranceNumber').type('0123456789');
+
+        cy.contains('Save and continue').click();
+
+        cy.get('h2').should('include.text', 'There is a problem');
+        cy.get('a').should('include.text', 'National Insurance Number should contain no more than 9 alphanumeric characters');
+    });
+
+    it('Allows a user to enter correct NI number after entering an incorrect one', () => {
+        cy.visit('/');
+        cy.get('h1').should('include.text', 'Check if your children can get free school meals');
+        cy.contains('Start Now').click();
+
+        cy.url().should('include', '/Check/Enter_Details');
+        cy.get('h1').should('include.text', 'Enter your details');
+        cy.get('#FirstName').type('Tim');
+        cy.get('#LastName').type('Smith');
+
+        cy.get('#Day').type('01');
+        cy.get('#Month').type('01');
+        cy.get('#Year').type('1990');
+
+        cy.get('#IsNassSelected').click();
+        cy.get('#NationalInsuranceNumber').type('0123456789');
+
+        cy.contains('Save and continue').click();
+
+        cy.get('h2').should('include.text', 'There is a problem');
+        cy.get('a').should('include.text', 'National Insurance Number should contain no more than 9 alphanumeric characters');
+
+        cy.get('#NationalInsuranceNumber').clear().type('AB123456C');
+        cy.contains('Save and continue').click();
+        cy.url().should('include','/Check/Loader');
+
+    })
+
 });
