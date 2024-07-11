e47a9dffc8ca760478595c988cbb2cbee7828f81
Sam Fallowfield <sam.fallowfield@education.gov.uk>
2024-07-11 11:57:29 +0100

    removed unnecessary comments

--- tests/cypress/e2e/EligibilityCheck/EnterChildDetails.spec.ts
+++ tests/cypress/e2e/EligibilityCheck/EnterChildDetails.spec.ts
@@ -5,19 +5,16 @@ describe('Parents can enter childs details successfully', () => {
         cy.visit('/Check/Enter_Child_Details');
         cy.get('h1').should('include.text', 'Provide details of your children');
 
-        // cy.typeIntoInput(enterChildDetailsPage.getFieldSelector("Child One last name"), "Smith");
+
         cy.get('[id="ChildList[0].FirstName"]').type('Tim');
         cy.get('[id="ChildList[0].LastName"]').type('Smith');
         cy.get('[id="ChildList[0].School.Name"]').type('Hinde House 2-16 Academy');
-        // cy.contains('Hinde House 2-16 Academy, 139856, S5 6AG, Sheffield').click();
+
         cy.get('#schoolList0')
             .should('be.visible')
             .contains('Hinde House 2-16 Academy, 139856, S5 6AG, Sheffield')
             .click({ force: true})
-            // .then(($el) => {
-            //     cy.wrap($el).scrollIntoView().should('be.visible');
-            //     cy.wrap($el).click({ force: true});
-            // });
+
         cy.get('[id="ChildList[0].Day"]').type('01');
         cy.get('[id="ChildList[0].Month"]').type('01');
         cy.get('[id="ChildList[0].Year"]').type('2007');
