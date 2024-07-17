f606311678ccadec94a2ff40aedc67a14cf25cd7
Sam Fallowfield <sam.fallowfield@education.gov.uk>
2024-07-17 17:05:13 +0100

    application controller changes

--- CheckYourEligibility-Admin/Controllers/ApplicationController.cs
+++ CheckYourEligibility-Admin/Controllers/ApplicationController.cs
@@ -1,23 +1,29 @@
+using CheckYourEligibility.Domain.Requests;
 using CheckYourEligibility_DfeSignIn;
 using CheckYourEligibility_DfeSignIn.Models;
 using CheckYourEligibility_FrontEnd.Models;
+using CheckYourEligibility_FrontEnd.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.IdentityModel.Tokens;
 using System.Diagnostics;
 using System.Security.Claims;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CheckYourEligibility_FrontEnd.Controllers
 {
     public class ApplicationController : BaseController
     {
         private readonly ILogger<ApplicationController> _logger;
+        private readonly IEcsServiceAdmin _service;
+        private readonly IEcsServiceAdmin _object;
 
-        public ApplicationController(ILogger<ApplicationController> logger)
+        public ApplicationController(ILogger<ApplicationController> logger, IEcsServiceAdmin ecsServiceAdmin)
         {
             
             _logger = logger;
-            
+            _service = ecsServiceAdmin ?? throw new ArgumentNullException(nameof(ecsServiceAdmin));
         }
 
         [HttpGet]
@@ -27,23 +33,27 @@ namespace CheckYourEligibility_FrontEnd.Controllers
         }
 
         [HttpPost]
-
-        public  Task<IActionResult> Search(ApplicationSearch request )
-       {
-            //Fetch result and pass it to view
-            var applicationSearch = new ApplicationSearch
+        public async Task<IActionResult> Results(ApplicationSearch request)
+        {
+            ApplicationRequestSearch applicationSearch = new ApplicationRequestSearch()
             {
-                LocalAuthority = request.LocalAuthority,
-                School = request.School,
-                Status = request.Status,
-                ChildLastName = request.ChildLastName,
-                ParentLastName = request.ParentLastName,
-                ReferenceNumber = request.ReferenceNumber,
-                ChildDateOfBirth = new DateOnly(request.ChildDOBYear.Value, request.ChildDOBMonth.Value, request.ChildDOBDay.Value).ToString("yyyy-MM-dd"),
-                ParentDateOfBirth = new DateOnly(request.PGDOBYear.Value, request.PGDOBMonth.Value, request.PGDOBDay.Value).ToString("yyyy-MM-dd")
+                Data = new ApplicationRequestSearchData
+                {
+
+                    //localAuthority = null,
+                    //School = _Claims.Organisation.Uid,
+                    //Status = request.Status,
+                    ChildLastName = request.ChildLastName,
+                    //ParentLastName = request.ParentLastName,
+                    //ReferenceNumber = request.ReferenceNumber, currently not in the model
+                    //ChildDateOfBirth = new DateOnly(request.ChildDOBYear.Value, request.ChildDOBMonth.Value, request.ChildDOBDay.Value).ToString("yyyy-MM-dd"),
+                    //ParentDateOfBirth = new DateOnly(request.PGDOBYear.Value, request.PGDOBMonth.Value, request.PGDOBDay.Value).ToString("yyyy-MM-dd")
+                }
             };
-            Console.WriteLine(applicationSearch.ChildLastName);
+            var response = await _service.PostApplicationSearch(applicationSearch);
+
+            //Fetch result and pass it to view
             return null;
-       }
-    }
+        }
+    } 
 }
