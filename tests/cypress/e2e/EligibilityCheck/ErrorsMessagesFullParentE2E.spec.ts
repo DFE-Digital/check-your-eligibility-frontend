4076b53f3f83f5ffa22df602a7c62d39c473eb1e
Sam Fallowfield <sam.fallowfield@education.gov.uk>
2024-07-23 15:09:18 +0100

    update

--- tests/cypress/e2e/EligibilityCheck/ErrorsMessagesFullParentE2E.spec.ts
+++ tests/cypress/e2e/EligibilityCheck/ErrorsMessagesFullParentE2E.spec.ts
@@ -73,13 +73,8 @@ describe('After errors have been input initially a Parent with valid details can
 
         cy.get('[id="ChildList[0].FirstName"]').type('123456');
         cy.get('[id="ChildList[0].LastName"]').type('123456');
-        cy.get('[id="ChildList[0].School.Name"]').type('Hinde House 2-16 Academy');
-
-        cy.get('#schoolList0')
-            .should('be.visible')
-            .contains('Hinde House 2-16 Academy, 139856, S5 6AG, Sheffield')
-            .click({ force: true})
-
+        cy.get('[id="ChildList[0].School.Name"]').clear();
+        
         cy.get('[id="ChildList[0].Day"]').clear().type('32');
         cy.get('[id="ChildList[0].Month"]').clear().type('13');
         cy.get('[id="ChildList[0].Year"]').clear().type('2050');
