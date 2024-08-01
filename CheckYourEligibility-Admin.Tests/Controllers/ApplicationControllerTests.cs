9407193d7d4979f7ec60a3f0dbefee6dd1a72145
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-08-01 16:03:52 +0100

    added context

--- CheckYourEligibility-Admin.Tests/Controllers/ApplicationControllerTests.cs
+++ CheckYourEligibility-Admin.Tests/Controllers/ApplicationControllerTests.cs
@@ -14,6 +14,9 @@ using AutoFixture;
 using AutoFixture.AutoMoq;
 using AutoFixture.Idioms;
 using CheckYourEligibility_FrontEnd.Models;
+using System.Security.Principal;
+using System.Security.Claims;
+using CheckYourEligibility_DfeSignIn.Models;
 
 namespace CheckYourEligibility_Parent.Tests.Controllers
 {
@@ -25,6 +28,7 @@ namespace CheckYourEligibility_Parent.Tests.Controllers
         private Mock<IEcsServiceAdmin> _adminServiceMock;
         private Mock<ISession> _sessionMock;
         private Mock<HttpContext> _httpContext;
+        private Mock<ClaimsPrincipal> _userMock;
         protected readonly Fixture _fixture = new Fixture();
 
         //private Mock<IConfiguration> _configMock;
@@ -41,6 +45,8 @@ namespace CheckYourEligibility_Parent.Tests.Controllers
         {
 
             SetUpInitialMocks();
+            SetUpSessionData();
+            SetClaimsData();
             SetUpHTTPContext();
             // SetUpServiceMocks();
 
@@ -56,14 +62,39 @@ namespace CheckYourEligibility_Parent.Tests.Controllers
             {
                 _httpContext = new Mock<HttpContext>();
                 _httpContext.Setup(ctx => ctx.Session).Returns(_sessionMock.Object);
+                _httpContext.Setup(ctx => ctx.User).Returns(_userMock.Object);
                 _sut.ControllerContext.HttpContext = _httpContext.Object;
             }
 
-            //void SetUpServiceMocks()
-            //{
-            //    _checkServiceMock.Setup(s => s.PostApplicationSearch(It.IsAny<ApplicationRequestSearch>()))
-            //        .ReturnsAsync(_applicationSearchResponse);
-            //}
+            void SetClaimsData()
+            {
+                _userMock = new Mock<ClaimsPrincipal>();
+                var claimSchool = new Claim("organisation", Properties.Resources.ClaimSchool);
+                var claim2 = new Claim($"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/{ClaimConstants.NameIdentifier}", Properties.Resources.ClaimSchool);
+                _userMock.Setup(x=>x.Claims).Returns(new List<Claim> { claimSchool,
+                    new Claim($"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/{ClaimConstants.NameIdentifier}", "123"),
+                    new Claim($"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress","test@test.com"),
+                    new Claim($"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname","testFirstName"),
+                    new Claim($"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname","testSurname")
+                });
+            }
+
+            void SetUpSessionData()
+            {
+                _sessionMock = new Mock<ISession>();
+                var sessionStorage = new Dictionary<string, byte[]>();
+
+                _sessionMock.Setup(s => s.Set(It.IsAny<string>(), It.IsAny<byte[]>()))
+                                .Callback<string, byte[]>((key, value) => sessionStorage[key] = value);
+
+                _sessionMock.Setup(s => s.TryGetValue(It.IsAny<string>(), out It.Ref<byte[]>.IsAny))
+                            .Returns((string key, out byte[] value) =>
+                            {
+                                var result = sessionStorage.TryGetValue(key, out var storedValue);
+                                value = storedValue;
+                                return result;
+                            });
+            }
         }
 
         [TearDown]
@@ -93,8 +124,6 @@ namespace CheckYourEligibility_Parent.Tests.Controllers
             _adminServiceMock.Setup(s => s.PostApplicationSearch(It.IsAny<ApplicationRequestSearch>()))
                    .ReturnsAsync(response);
 
-            _httpContext.Setup(s => s.)
-            
 
             var request = _fixture.Create<ApplicationSearch>();
 
