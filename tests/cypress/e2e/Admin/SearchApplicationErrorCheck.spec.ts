e571636175ce1149bbde3635df60ddbf45866bcc
Sam Fallowfield <sam.fallowfield@education.gov.uk>
2024-08-13 11:51:28 +0100

    test update

---
+++ tests/cypress/e2e/Admin/SearchApplicationErrorCheck.spec.ts
@@ -0,0 +1,40 @@
+
+describe('Admin journey search for application', () => {
+    beforeEach(() => {
+        cy.SignIn();
+        cy.contains('Search all records').click();
+
+    });
+
+    it('Returns the correct warning message when invalid characters are used in the Child last name field', () => {
+        
+        cy.get('#ChildLastName').type('12345');
+        cy.contains('Generate results').click();
+        cy.get('h2').should('include.text', 'There is a problem');
+
+        cy.get('li').should('include.text', 'Child Last Name field contains an invalid character');
+
+    });
+
+    it('Returns the correct warning message when invalid characters are used in the Parent last name field', () => {
+        
+        cy.get('#ParentLastName').type('12345');
+        cy.contains('Generate results').click();
+        cy.get('h2').should('include.text', 'There is a problem');
+
+        cy.get('li').should('include.text', 'Parent Last Name field contains an invalid character');
+
+    });
+
+    it('Returns the correct warning message when an invalid Parent last name is input', () => {
+        
+        cy.get('#ParentLastName').type('12345');
+        cy.contains('Generate results').click();
+        cy.get('h2').should('include.text', 'There is a problem');
+
+        cy.get('li').should('include.text', 'Parent Last Name field contains an invalid character');
+
+    });
+
+
+});
