ee1cbdfe6c6a89ea96fc4928c292e713e7704726
HANNON-HEALY, Conor <Conor.HANNON-HEALY@EDUCATION.GOV.UK>
2024-09-03 11:05:45 +0100

    admin working on C's machine

--- CheckYourEligibility-Admin/wwwroot/js/focusChildNameInput.js
+++ CheckYourEligibility-Admin/wwwroot/js/focusChildNameInput.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Select all input fields with IDs starting with 'ChildList' and ending with 'FirstName'
-    var inputs = document.querySelectorAll("input[id^='ChildList'][id$='FirstName']");
-    // Focus on the last input field
-    if (inputs.length > 0) {
-        inputs[inputs.length - 1].focus();
-    }
-});
+//document.addEventListener("DOMContentLoaded", function () {
+//    // Select all input fields with IDs starting with 'ChildList' and ending with 'FirstName'
+//    var inputs = document.querySelectorAll("input[id^='ChildList'][id$='FirstName']");
+//    // Focus on the last input field
+//    if (inputs.length > 0) {
+//        inputs[inputs.length - 1].focus();
+//    }
+//});
