bb4c2e2e3a661891a2d147c89417b0cdda289de1
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-09-11 11:39:40 +0100

    update batch template

--- CheckYourEligibility-Admin/Models/BatchFSMExport.cs
+++ CheckYourEligibility-Admin/Models/BatchFSMExport.cs
@@ -4,20 +4,18 @@ namespace CheckYourEligibility_FrontEnd.Models
 {
     public class BatchFSMExport
     {
-        [Name("Parent First Name")]
-        public string FirstName { get; set; }
-
-        [Name("Parent Last Name")]
-        public string LastName { get; set; }
-
-        [Name("Parent Date of Birth")]
-        public string DOB { get; set; }
 
         [Name("Parent NI Number")]
         public string NI { get; set; }
 
         [Name("Parent Asylum Seeker Reference Number")]
         public string NASS { get; set; }
+
+        [Name("Parent Date of Birth")]
+        public string DOB { get; set; }
+
+        [Name("Parent Last Name")]
+        public string LastName { get; set; }
 
         [Name("Outcome")]
         public string Outcome { get; set; }
