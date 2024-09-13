54ce3ae428d2604afef7c2be85af76e34daa767f
HANNON-HEALY, Conor <Conor.HANNON-HEALY@EDUCATION.GOV.UK>
2024-09-13 14:34:09 +0100

    Telemetry initialiser for admin and parent

--- CheckYourEligibility-Admin/Program.cs
+++ CheckYourEligibility-Admin/Program.cs
@@ -1,21 +1,28 @@
 using CheckYourEligibility_FrontEnd;
 using Azure.Identity;
 using CheckYourEligibility_DfeSignIn;
+using Microsoft.ApplicationInsights.Extensibility;
+using CheckYourEligibility_FrontEnd.Telemetry;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+builder.Services.AddServices(builder.Configuration);
+
+// Add Application Insights telemetry
 builder.Services.AddApplicationInsightsTelemetry();
-if (Environment.GetEnvironmentVariable("KEY_VAULT_NAME")!=null)
+
+builder.Services.AddSession();
+
+if (Environment.GetEnvironmentVariable("KEY_VAULT_NAME") != null)
 {
     var keyVaultName = Environment.GetEnvironmentVariable("KEY_VAULT_NAME");
     var kvUri = $"https://{keyVaultName}.vault.azure.net";
 
     builder.Configuration.AddAzureKeyVault(new Uri(kvUri), new DefaultAzureCredential());
 }
-
-// Add services to the container.
-builder.Services.AddServices(builder.Configuration);
-builder.Services.AddSession();
 
 var dfeSignInConfiguration = new DfeSignInConfiguration();
 builder.Configuration.GetSection("DfeSignIn").Bind(dfeSignInConfiguration);
@@ -24,14 +31,35 @@ builder.Services.AddDfeSignInAuthentication(dfeSignInConfiguration);
 //builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 //builder.Services.AddProblemDetails();
 
-
 var app = builder.Build();
+
+// Add middleware to enable response body buffering
+app.Use(async (context, next) =>
+{
+    var originalBodyStream = context.Response.Body;
+
+    using (var responseBody = new MemoryStream())
+    {
+        context.Response.Body = responseBody;
+
+        await next();
+
+        context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+        // Optional: Read the response body if needed
+        // string responseText = await new StreamReader(context.Response.Body).ReadToEndAsync();
+
+        context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+        await responseBody.CopyToAsync(originalBodyStream);
+    }
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   
     app.UseHsts();
 }
 
@@ -40,8 +68,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); 
 app.UseAuthorization();
 app.UseSession();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
