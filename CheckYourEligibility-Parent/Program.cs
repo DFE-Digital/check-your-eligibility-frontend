356081e77e8268a210535931c91eb7e0918027d8
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-09-17 13:26:03 +0100

    Logging

--- CheckYourEligibility-Parent/Program.cs
+++ CheckYourEligibility-Parent/Program.cs
@@ -5,6 +5,7 @@ using GovUk.OneLogin.AspNetCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Cryptography;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -82,11 +83,47 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseSession();
 app.UseRouting();
-app.UseMiddleware<ResponseBodyLoggingMiddleware>();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Use(async (httpContext, next) =>
+{
+    try
+    {
+        httpContext.Request.EnableBuffering();
+        string requestBody = await new StreamReader(httpContext.Request.Body, Encoding.UTF8).ReadToEndAsync();
+        httpContext.Request.Body.Position = 0;
+        app.Logger.LogInformation($"Request body: {requestBody}");
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogInformation($"Exception reading request: {ex.Message}");
+    }
+
+    Stream originalBody = httpContext.Response.Body;
+    try
+    {
+        using var memStream = new MemoryStream();
+        httpContext.Response.Body = memStream;
+
+        // call to the following middleware 
+        // response should be produced by one of the following middlewares
+        await next(httpContext);
+
+        memStream.Position = 0;
+        string responseBody = new StreamReader(memStream).ReadToEnd();
+
+        memStream.Position = 0;
+        await memStream.CopyToAsync(originalBody);
+        app.Logger.LogInformation($"Response body: {responseBody}");
+    }
+    finally
+    {
+        httpContext.Response.Body = originalBody;
+    }
+});
 
 app.Run();
