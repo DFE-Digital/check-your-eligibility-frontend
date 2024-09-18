a4dbc37dd6522583f6dc531953ef6f8df89af351
Sam Fallowfield <sam.fallowfield@education.gov.uk>
2024-09-18 16:07:27 +0100

    fix issue with uncaught exception breaking local run cypress tests

--- tests/cypress/support/e2e.ts
+++ tests/cypress/support/e2e.ts
@@ -16,5 +16,14 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+Cypress.on('uncaught:exception', (err, runnable) => {
+    if(
+        err.message.includes("Cannot read properties of null (reading 'postMessage')") ||
+        err.message.includes('PrimaryOriginCommunicator')
+    ) {
+        return false;
+    }
+});
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
