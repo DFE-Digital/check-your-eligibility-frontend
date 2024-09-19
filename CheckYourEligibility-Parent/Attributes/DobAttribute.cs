9a5706fe53f98f255f877906182a3a5087534e32
Sam Fallowfield <sam.fallowfield@education.gov.uk>
2024-09-19 09:57:58 +0100

    .

--- CheckYourEligibility-Parent/Attributes/DobAttribute.cs
+++ CheckYourEligibility-Parent/Attributes/DobAttribute.cs
@@ -16,7 +16,6 @@ public class DobAttribute : ValidationAttribute
         var monthString = monthProperty?.GetValue(model) as string;
         var yearString = yearProperty?.GetValue(model) as string;
 
-        // Check if all date parts are provided
         if (string.IsNullOrWhiteSpace(dayString) && string.IsNullOrWhiteSpace(monthString) && string.IsNullOrWhiteSpace(yearString))
         {
             return new ValidationResult("Enter a date of birth", new[] { "DateOfBirth" });
@@ -26,7 +25,6 @@ public class DobAttribute : ValidationAttribute
             return new ValidationResult("Enter a complete date of birth", new[] { "DateOfBirth" });
         }
 
-        // Parse the day
         if (!int.TryParse(dayString, out int dayInt))
         {
             return new ValidationResult("Enter a day using numbers only", new[] { "DateOfBirth" });
@@ -37,7 +35,6 @@ public class DobAttribute : ValidationAttribute
             return new ValidationResult("Enter a valid day", new[] { "DateOfBirth" });
         }
 
-        // Parse the month
         if (!int.TryParse(monthString, out int monthInt))
         {
             return new ValidationResult("Enter a month using numbers only", new[] { "DateOfBirth" });
@@ -48,7 +45,6 @@ public class DobAttribute : ValidationAttribute
             return new ValidationResult("Enter a valid month", new[] { "DateOfBirth" });
         }
 
-        // Parse the year
         if (!int.TryParse(yearString, out int yearInt))
         {
             return new ValidationResult("Enter a year using numbers only", new[] { "DateOfBirth" });
@@ -56,7 +52,6 @@ public class DobAttribute : ValidationAttribute
 
         var dob = new DateTime(yearInt, monthInt, dayInt);
 
-        // Check if the date is in the future
         if (dob > DateTime.Now)
         {
             return new ValidationResult("Enter a date in the past", new[] { "DateOfBirth" });
@@ -67,16 +62,11 @@ public class DobAttribute : ValidationAttribute
             return new ValidationResult("Enter a valid year", new[] { "DateOfBirth" });
         }
 
-        // Check if the day is valid for the given month and year
         if (dayInt > DateTime.DaysInMonth(yearInt, monthInt))
         {
             return new ValidationResult("Enter a valid day", new[] { "DateOfBirth" });
         }
 
-        // Construct the DateTime object
-
-
-        // Additional validation for Child model (if applicable)
         if (model.GetType().Name == "Child")
         {
             int age = CalculateAge(dob, DateTime.Now);
