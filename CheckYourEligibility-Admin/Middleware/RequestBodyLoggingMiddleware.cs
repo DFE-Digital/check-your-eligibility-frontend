d1bbcbf8707654d5fcef95ebd2716292c9a8d38e
Richard CRITTENDEN <Richard.CRITTENDEN@EDUCATION.GOV.UK>
2024-10-04 13:11:26 +0100

    added logging

---
+++ CheckYourEligibility-Admin/Middleware/RequestBodyLoggingMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.ApplicationInsights.DataContracts;
+using System.Text;
+
+namespace CheckYourEligibility_FrontEnd.Middleware
+{
+    public class RequestBodyLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestBodyLoggingMiddleware> _logger;
+
+        public RequestBodyLoggingMiddleware(RequestDelegate next, ILogger<RequestBodyLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Request.EnableBuffering(); // Allow multiple reads
+
+            string requestBody = string.Empty;
+            if (context.Request.ContentLength > 0 &&
+                context.Request.ContentType != null &&
+                context.Request.ContentType.Contains("application/json"))
+            {
+                using (var reader = new StreamReader(
+                    context.Request.Body,
+                    encoding: Encoding.UTF8,
+                    detectEncodingFromByteOrderMarks: false,
+                    bufferSize: 1024,
+                    leaveOpen: true))
+                {
+                    requestBody = await reader.ReadToEndAsync();
+                    context.Request.Body.Position = 0; // Reset stream position
+                }
+
+                // Log the request body
+                _logger.LogInformation($"Endpoint:{context.GetEndpoint()} Request Body: {requestBody} endpoint");
+
+                // Optionally, attach to telemetry
+                var telemetry = context.Features.Get<RequestTelemetry>();
+                if (telemetry != null)
+                {
+                    telemetry.Name = "EceAdminRequest";
+                    telemetry.Properties["RequestBody"] = requestBody;
+                }
+            }
+
+            await _next(context);
+        }
+    }
+}
