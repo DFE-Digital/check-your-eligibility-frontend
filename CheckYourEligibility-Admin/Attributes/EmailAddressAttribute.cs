6cf6d2fa06cf71e96372ec6476ed90d84d6bcddd
Harrison SHAW <Harrison.SHAW@EDUCATION.GOV.UK>
2024-11-07 10:45:05 +0000

    New attribute for EmailAddress for custom validation

---
+++ CheckYourEligibility-Admin/Attributes/EmailAddressAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class CustomEmailAddressAttribute : ValidationAttribute
+{
+    private static bool EnableFullDomainLiterals { get; } =
+        AppContext.TryGetSwitch("System.Net.AllowFullDomainLiterals", out bool enable) ? enable : false;
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value == null)
+        {
+            return ValidationResult.Success;
+        }
+
+        if (!(value is string valueAsString))
+        {
+            return new ValidationResult("Invalid email address format.");
+        }
+
+        if (!EnableFullDomainLiterals && (valueAsString.Contains('\r') || valueAsString.Contains('\n')))
+        {
+            return new ValidationResult("Please enter a valid email address.");
+        }
+
+        int index = valueAsString.IndexOf('@');
+        if (index <= 0 || index == valueAsString.Length - 1 || index != valueAsString.LastIndexOf('@'))
+        {
+            return new ValidationResult("Please enter a valid email address.");
+        }
+
+        return ValidationResult.Success;
+    }
+}
