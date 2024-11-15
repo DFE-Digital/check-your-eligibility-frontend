d08a19c74cac68c8a86e8e1dcaaeb3b4e5c42377
marten.wetterberg <marten.wetterberg@nyweb.nu>
2024-11-15 12:21:48 +0000

    Update security check cypress

--- tests/cypress/e2e/Admin/SecurityChecks.spec.ts
+++ tests/cypress/e2e/Admin/SecurityChecks.spec.ts
@@ -21,7 +21,7 @@ describe("Admin Portal pages contain recommended network security related header
             url: '/home/error'
         }).then((response) => {
             console.log(response.headers)
-            expect(response.headers).to.have.property('content-security-policy', `default-src 'self'`);
+            expect(response.headers).to.have.property('content-security-policy', `default-src 'self' https://*.clarity.ms https://c.bing.com`);
             expect(response.headers).to.have.property('strict-transport-security', 'max-age=31536000; includeSubDomains');
             expect(response.headers).to.have.property('x-xss-protection', '1; mode=block');
             expect(response.headers).to.have.property('x-content-type-options', 'nosniff');
