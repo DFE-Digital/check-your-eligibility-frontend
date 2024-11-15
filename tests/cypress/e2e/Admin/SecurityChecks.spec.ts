b60e51aabe21d5def54b273dfe7533edf12bcdf5
marten.wetterberg <marten.wetterberg@nyweb.nu>
2024-11-15 11:54:43 +0000

    Last PR review for Sam

--- tests/cypress/e2e/Admin/SecurityChecks.spec.ts
+++ tests/cypress/e2e/Admin/SecurityChecks.spec.ts
@@ -9,7 +9,7 @@ describe("Admin Portal pages contain recommended network security related header
             url: '/'
         }).then((response) => {
             console.log(response.headers)
-            expect(response.headers).to.have.property('content-security-policy', `default-src 'self'`);
+            expect(response.headers).to.have.property('content-security-policy', `default-src 'self' https://*.clarity.ms https://c.bing.com`);
             expect(response.headers).to.have.property('strict-transport-security', 'max-age=31536000; includeSubDomains');
             expect(response.headers).to.have.property('x-xss-protection', '1; mode=block');
             expect(response.headers).to.have.property('x-content-type-options', 'nosniff');
@@ -28,4 +28,45 @@ describe("Admin Portal pages contain recommended network security related header
         })
         })
     }
-)
+);
+
+describe('Clarity', () => {
+    var schoolClarityId: string
+    var LAClarityId: string
+
+    it('Loads Clarity when it is enabled', () => {
+        cy.SignInSchool();
+
+        cy.get('body')
+            .invoke('attr', 'data-clarity')
+            .then(($clarity) => {
+                    if($clarity)
+                        schoolClarityId = $clarity;
+                    cy.get('head script[src*="clarity"]');
+                }
+            );
+    });
+
+    it('Does not Clarity when it is disabled', () => {
+        cy.SignInLA();
+
+        cy.get('body')
+            .invoke('attr', 'data-clarity')
+            .then(($clarity) => {
+                    if(!$clarity) {
+                        cy.get('head script[src*="clarity"]').should('not.exist');
+                    }
+
+                    else {
+                        LAClarityId = $clarity;
+                    }
+                }
+            );
+    });
+
+    it('Loads different Clarity for School and LA', () => {
+        if(schoolClarityId) {
+            expect(schoolClarityId).not.equal(LAClarityId)
+        }
+    });
+});
---
+++ tests/cypress/e2e/EligibilityCheck/SecurityChecks.spec.ts
@@ -0,0 +1,56 @@
+describe("Parent Portal pages contain recommended network security related headers", () => {
+    it("Pages respond with recommended network security headers", () => {
+        // check for network headers on a regular page
+        cy.request({
+            method: 'GET',
+            url: '/'
+        }).then((response) => {
+            console.log(response.headers)
+            expect(response.headers).to.have.property('content-security-policy', `default-src 'self' https://*.clarity.ms https://c.bing.com`);
+            expect(response.headers).to.have.property('strict-transport-security', 'max-age=31536000; includeSubDomains');
+            expect(response.headers).to.have.property('x-xss-protection', '1; mode=block');
+            expect(response.headers).to.have.property('x-content-type-options', 'nosniff');
+        })
+    
+        // check for network headers on a error page
+        cy.request({
+            method: 'GET',
+            url: '/home/error'
+        }).then((response) => {
+            console.log(response.headers)
+            expect(response.headers).to.have.property('content-security-policy', `default-src 'self'`);
+            expect(response.headers).to.have.property('strict-transport-security', 'max-age=31536000; includeSubDomains');
+            expect(response.headers).to.have.property('x-xss-protection', '1; mode=block');
+            expect(response.headers).to.have.property('x-content-type-options', 'nosniff');
+        })
+        })
+    }
+);
+
+describe('Clarity', () => {
+    it('Loads Clarity when it is enabled', () => {
+        cy.visit("/");
+
+        cy.get('body')
+            .invoke('attr', 'data-clarity')
+            .then(($clarity) => {
+                    if($clarity) {
+                        cy.get('head script[src*="clarity"]');
+                    }
+                }
+            );
+    });
+
+    it('Does not Clarity when it is disabled', () => {
+        cy.visit("/");
+
+        cy.get('body')
+            .invoke('attr', 'data-clarity')
+            .then(($clarity) => {
+                    if(!$clarity) {
+                        cy.get('head script[src*="clarity"]').should('not.exist');
+                    }
+                }
+            );
+    });
+});
