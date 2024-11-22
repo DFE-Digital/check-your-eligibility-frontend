c2baae53b2278fb516635b780c6c62ad81a495ed
marten.wetterberg <marten.wetterberg@nyweb.nu>
2024-11-22 17:56:38 +0000

    Add checks for successful bulk upload

--- tests/cypress/e2e/Admin/AdminBulkCheckJourney.spec.ts
+++ tests/cypress/e2e/Admin/AdminBulkCheckJourney.spec.ts
@@ -1,8 +1,16 @@
 
 describe("Admin Portal journey for checking parent eligibility using the Bulk Checking Service", () => {
+    beforeEach(() => {
+        cy.session("Session 0", () => {
+            cy.SignInSchool();
 
+            cy.wait(1);
+        });
+    });
+    
     it("will return an error message if the bulk file contains more than 250 rows of data", () => {
-        cy.SignInSchool()
+        cy.visit("/");
+        
         cy.contains('Run a batch check').click()
         cy.get('input[type=file]').selectFile('bulkchecktemplate_too_many_records.csv')
         cy.contains('Run check').click()
@@ -15,7 +23,8 @@ describe("Admin Portal journey for checking parent eligibility using the Bulk Ch
     }) 
 
     it("will return an error message if more than 10 batches are attempted within an hour", () => {
-        cy.SignInSchool()
+        cy.visit("/");
+        
         cy.contains('Run a batch check').click()
         for (let i = 0; i < 11; i++) {
             cy.get('input[type=file]').selectFile('bulkchecktemplate_too_many_records.csv')
@@ -26,5 +35,18 @@ describe("Admin Portal journey for checking parent eligibility using the Bulk Ch
         cy.get('@errorMessage').should(($p) => {
             expect($p.first()).to.contain('No more than 10 bulk check requests can be made per hour')
         })
+    })
+
+
+    it("will run a successful batch check", () => {
+        cy.visit("/");
+
+        cy.contains('Run a batch check').click()
+        cy.get('input[type=file]').selectFile('bulkchecktemplate_complete.csv')
+        cy.contains('Run check').click();
+
+        cy.get('h1', { timeout: 80000 }).should('include.text', 'Checks completed');
+        
+        cy.contains("Download").click();
     })
 })
